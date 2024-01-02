7903c1be02c3345208ee84dcce1124ef79b61d6f
brion4 <bharathsatheesan@gmail.com>
2024-01-02 21:00:38 +0530

    Update UI colors and styles in main.dart and verify_email_view.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:e_commerce_flutter/core/binding.dart';
 import 'package:e_commerce_flutter/core/dependency.dart';
 import 'package:e_commerce_flutter/firebase_options.dart';
 import 'package:e_commerce_flutter/src/auth/forgot_password/forgot_password_screen.dart';
-import 'package:e_commerce_flutter/src/auth/login/login_screen.dart';
 import 'package:e_commerce_flutter/src/auth/verify_email/verify_email_view.dart';
 import 'package:e_commerce_flutter/src/customerview/view/screen/home_screen/home_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -38,41 +36,4 @@ void main() async {
       initialBinding: DashBoardControllerBinding(),
     ),
   );
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
-            if (user != null) {
-              if (user.emailVerified) {
-                // Display the message and redirect to login screen
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil('/login/', (route) => false);
-                });
-                return const Scaffold(
-                  body: Center(child: Text("Email is verified")),
-                );
-              } else {
-                return const VerifyEmailView();
-              }
-            } else {
-              return LoginScreen();
-            }
-          default:
-            return const Center(child: CircularProgressIndicator.adaptive());
-        }
-      },
-    );
-  }
 }
